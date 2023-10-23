0936a7c06f8653960186f1bf4ca3be6165b27b40
riccardococciolo <coccioloriccardo64@gmail.com>
2023-10-23 16:56:04 +0200

    add: logic

--- us-dice/js/script.js
+++ us-dice/js/script.js
@@ -3,3 +3,15 @@ console.log(userNumber);
 
 const computerNumber = (Math.floor(Math.random() * 6)) + 1;
 console.log(computerNumber);
+
+let result = "";
+
+if (userNumber > computerNumber) {
+    result = "Hai vinto!!!"
+} else if (userNumber < computerNumber) {
+    result = "Hai perso =("
+} else {
+    result = "Pareggio, oggi sei fortunato."
+}
+
+console.log(result);
